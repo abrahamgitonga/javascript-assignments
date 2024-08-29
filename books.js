e7b9168b18ae2edcbd37ab078d9c747cc386fc79
abrahamgitonga <abrahamgitonga98@gmail.com>
2024-08-29 10:11:45 +0300

    initial

---
+++ books.js
@@ -0,0 +1,13 @@
+const book = {
+    title: 'javascript essentials',
+    author: 'john doe',
+    address: {
+        street: '123 main st',
+        city: 'anytown',
+        state: 'ca',
+        
+    }
+};
+
+console.log("authors name:", book.author);
+console.log("authors street:", book.address.street);
